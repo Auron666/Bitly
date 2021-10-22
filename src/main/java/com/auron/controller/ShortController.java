a73a197c3b5f8b0ac510c263739b6db0ccb71a89
Auron666 <trojian8@gmail.com>
2021-10-22 12:46:05 +0300

    Добавил генерацию коротких ссылок, добавление hash в БД

--- src/main/java/com/auron/controller/ShortController.java
+++ src/main/java/com/auron/controller/ShortController.java
@@ -3,26 +3,48 @@ package com.auron.controller;
 import com.auron.model.ShortUrl;
 import com.auron.repository.ShortRepository;
 
+import com.auron.utils.CodeGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.time.ZonedDateTime;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @Slf4j
 public class ShortController {
 
+    @Value("${shorturl.length}")
+    private Integer shorterLength;
+
     @Autowired
     private ShortRepository shortRepository;
 
-    @PostMapping(path = "/")
-    public String createShortUrl(String original){
-        return null;
+    @Autowired
+    private CodeGenerator codeGenerator;
+
+    @PostMapping(path = "/", consumes = APPLICATION_JSON_VALUE)
+    public ShortUrl createShortUrl(@RequestBody ShortUrl shortUrl){
+
+        String hash = codeGenerator.generate(shorterLength);
+
+        if(shortUrl != null){
+
+            String shorterString = URLDecoder.decode(shortUrl.getOriginalUrl());
+
+            shortUrl = new ShortUrl(null, hash, shorterString, ZonedDateTime.now());
+
+            return shortRepository.save(shortUrl);
+        }else{
+            return null;
+        }
     }
 
     @GetMapping(path = "/{hash}")
